5cc0808e6d6074d9791219c08651fe235ee6cebb
Hui lala <ctolentino@myhta.org>
2019-01-29 15:12:07 -1000

    updated exercises

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Fork and clone this repository to practice Arrays after learning it for the first time.
 
 Set-up
-1. Create an html and css file for this repo.  Make sure all the files are linked.
+1. Create a css file for this repo.  Make sure the html & css files are linked.
 2. Follow the instructions in the javascript file to complete your work.
 3. Commit and push your repo back up to github.
 4. Paste the link to this repo in slack.
---
+++ app.js
@@ -0,0 +1,17 @@
+//1. Create an array listing ingredients to your favorite recipe.  
+//Name your array the recipe name.
+//2. Create an array listing the classes you are taking this semester.
+//Name your array "SpringSem19"
+//3. Create an array listing your favorite all time movies.
+//Name your array "FaveMovies"
+//4. Create an array listing any other "favorites" you have.
+//Name your array the category it is.
+//5. Access the second element in your recipe array and print it as part of the following sentence 
+//"The second ingredient to my favorite recipe is ________."
+//6. Access the third element in your class list array and print it as part of the following sentence 
+//"One of the classes I'm taking this semester is ________."
+//7. Access the first element in your movie list array and print it as part of the following sentence 
+//"One of my favorite movies is ________."
+//8. Access the first element in your last  array and print it as part of the following sentence 
+//"One of my favorite __(fill in)__ is ________."
+
